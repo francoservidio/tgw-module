e53143a11ed3fdd6404de57b2f5576db5a8630aa
Franco Servidio <franco@MAC-0495.local>
2022-02-07 17:26:27 -0300

    new outputs

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "ec2_transit_gateway_arn" {
     description = "EC2 Transit Gateway Amazon Resource Name (ARN)"
     value       = element(concat(aws_ec2_transit_gateway.tgw.*.arn, [""]), 0)
 }
-output "aws_ec2_transit_gateway_route_table" {
-  description = "Identifier of the default association route table"
-  value       = element(concat(aws_ec2_transit_gateway_route_table.tgw_route_table.*.id, [""]), 0)
-}
 output "ec2_transit_gateway_id" {
   description = "EC2 Transit Gateway identifier"
   value       = element(concat(aws_ec2_transit_gateway.tgw.*.id, [""]), 0)
@@ -13,10 +9,6 @@ output "ec2_transit_gateway_id" {
 output "ec2_transit_gateway_owner_id" {
   description = "Identifier of the AWS account that owns the EC2 Transit Gateway"
   value       = element(concat(aws_ec2_transit_gateway.tgw.*.owner_id, [""]), 0)
-}
-output "ec2_transit_gateway_route_table_default_association_route_table" {
-  description = "Boolean whether this is the default association route table for the EC2 Transit Gateway"
-  value       = element(concat(aws_ec2_transit_gateway_route_table.tgw_route_table.*.default_association_route_table, [""]), 0)
 }
 # aws_ram_resource_share
 output "ram_resource_share_id" {
