018ac15e7690b60c95c224e04ae07fa7925fcc32
Franco Servidio <franco@MAC-0495.local>
2022-02-07 16:18:10 -0300

    first commit

---
+++ account-networking.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  alias = "account-ipsy-us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::<<account-number>>:role/terraform-admin-trusting"
+  }
+  ignore_tags {
+    key_prefixes = ["kubernetes.io/"]
+  }
+  region  = "us-east-1"
+  profile = "lockdown"
+}
+#
+resource "aws_ec2_transit_gateway" "tgw" {
+  count = var.create_tgw ? 1 : 0
+
+  description = var.tgw_gateway_description
+
+  amazon_side_asn                 = var.tgw_amazon_side_asn
+  auto_accept_shared_attachments  = var.tgw_auto_accept_shared_attachments
+  default_route_table_association = var.tgw_default_route_table_association
+  default_route_table_propagation = var.tgw_default_route_table_propagation
+  dns_support                     = var.tgw_dns_support
+
+  vpn_ecmp_support = var.tgw_vpn_ecmp_support
+  tags             = var.tgw_tags
+}
+
+# Creates a Transit gateway route table and associates with the Transit Gateway
+resource "aws_ec2_transit_gateway_route_table" "tgw_route_table" {
+  for_each = var.tgw_tags_name_rt
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+
+  tags = {
+    Name = each.key
+  }
+}
+##########################
+# Resource Access Manager
+##########################
+resource "aws_ram_resource_share" "tgw_ram" {
+  count = var.create_tgw && var.share_tgw ? 1 : 0
+
+  name                      = var.ram_name
+  allow_external_principals = var.ram_allow_external_principals
+
+  tags = {
+      "Name" = var.ram_name
+    }
+}
+resource "aws_ram_resource_association" "tgw_ram_association" {
+  count = var.create_tgw && var.share_tgw ? 1 : 0
+
+  resource_arn       = aws_ec2_transit_gateway.tgw[0].arn
+  resource_share_arn = aws_ram_resource_share.tgw_ram[0].id
+}
+resource "aws_ram_resource_share_accepter" "this" {
+  count = !var.create_tgw && var.share_tgw ? 1 : 0
+
+  share_arn = var.ram_resource_share_arn
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "create_tgw" {
+  description = "Create a transit gateway or not"
+  default     = false
+}
+variable "tgw_gateway_description" {
+  description = "Description of the Gateway"
+  default     = ""
+}
+variable "tgw_amazon_side_asn" {
+  description = "ASN of AWS Side of a BGP Session"
+  default     = "64512"
+}
+variable "tgw_auto_accept_shared_attachments" {
+  description = "Whether resource requests are automatically accepted"
+  default     = "disable"
+}
+variable "tgw_default_route_table_association" {
+  description = "Whether resource attachments are automatically associated with the default association route table"
+  default     = "disable"
+}
+variable "tgw_default_route_table_propagation" {
+  description = "Whether resource attachments automatically propagate routes to the default propagation route table"
+  default     = "disable"
+}
+variable "tgw_dns_support" {
+  description = "Whether DNS support is enabled"
+  default     = "enable"
+}
+variable "tgw_vpn_ecmp_support" {
+  description = "Whether VPN Equal Cost Multipath protocol is enabled"
+  default     = "disable"
+}
+variable "tgw_tags" {
+  description = "Key Value Tags for the EC2 Transit Gateway"
+  default     = {}
+
+  type = map(string)
+}
+variable "tgw_tags_name_rt" {
+  description = "Name Tag for the EC2 Transit Gateway Route Table"
+  default     = []
+}
+variable "share_tgw" {
+  description = "Whether to share your transit gateway with other accounts"
+  type        = bool
+  default     = false
+}
+variable "ram_name" {
+  description = "The name of the resource share of TGW"
+  type        = string
+  default     = ""
+}
+variable "ram_allow_external_principals" {
+  description = "Indicates whether principals outside your organization can be associated with a resource share."
+  type        = bool
+  default     = false
+}
+variable "ram_resource_share_arn" {
+  description = "ARN of RAM resource share"
+  type        = string
+  default     = ""
+}
